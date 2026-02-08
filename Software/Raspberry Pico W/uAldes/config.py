6d10c1dd51556da2f663772902e57daa6065e4a7
Fredzxda <37531742+Fredzxda@users.noreply.github.com>
2026-02-08 19:38:02 +0100

    First version taking into account EASYHOME VMC

--- Software/Raspberry Pico W/uAldes/config.py
+++ Software/Raspberry Pico W/uAldes/config.py
@@ -3,27 +3,274 @@
 
 # WiFi Configuration
 WIFI_NETWORKS ={
-        "ssid": "your_wifi_ssid",
-        "password": "your_wifi_password"
+        "ssid": "xxxxxxxx",
+        "password": "xxxxxxxx"
     }
+
+# ALDES
+UALDES_OPTIONS = {  
+    "refresh_time": 60, # Time in seconds to refresh data
+    "device": "EASYHOME", #T.FLOW or EASYHOME
+    "serial_number": "ualdes",
+    "device_in_ha": True
+}
 
 # MQTT Configuration
 MQTT_CONFIG = {
-    "broker": "mqtt.broker.address",
+    "broker": "xxx.xxx.xxx.xxx",
     "port": 1883,
     "client_id": "aldes",
-    "user": "mqtt_username",
-    "password": "mqtt_password",
+    "user": "aldes",
+    "password": "aldes",
     "ssl": False,
-    "keepalive": 60
+    "keepalive": 30
 }
 
 # MQTT Topics
 MQTT_TOPICS = {
     "main": "aldes/",
-    "command": "aldes/commands",
+    "command": "aldes/"+UALDES_OPTIONS["serial_number"]+"/set_mode",
+    "haPrefix": "homeassistant/",
 }
 
-UALDES_OPTIONS = {  
-    "refresh_time": 60 # Time in seconds to refresh data
+# MQTT Topics
+MQTT_GROUPS = {
+    "mode": {"main": "Mode", "conve": "Mode"},
+    "qai": {"pwmqai": "PwmQAI_1", "co2": "CO2", "varhr": "Hum_Variat"},
+    "kitchen": {"tmpcu": "Temp_Kitchen","hrcu": "Hum_Kitchen"},
+    "bathroom": {"tmpba1": "Temp_Bath1","hrba1": "Hum_Bath1", "tmpba2": "Temp_Bath2","hrba2": "Hum_Bath2"},
+}
+
+# HA DEVICE Information
+MQTT_HA = {
+    "main_mode_select": {
+        "type": "select",
+        "properties": {
+            "name": "Main mode",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_main_mode_selector",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_main_mode",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/mode",
+            "value_template": "{{ value_json.main }}",
+            "command_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/set_mode",
+            "options": ["Daily", "Guest", "Boost", "Holiday", "Programming"],
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "main_mode": {
+        "type": "sensor",
+        "properties": {
+            "name": "Main mode",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_main_mode",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_main_mode",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/mode",
+            "value_template": "{{ value_json.main }}",
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "pwmqai": {
+        "type": "sensor",
+        "properties": {
+            "name": "PwmQai",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_pwmqai",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_pwmqai",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/qai",
+            "value_template": "{{ value_json.pwmqai }}",
+            "min": 0,
+            "max": 1000,
+            "step": 1,
+            "qos": 1,
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "co2": {
+        "type": "sensor",
+        "properties": {
+            "name": "PwmQai",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_co2",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_co2",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/qai",
+            "value_template": "{{ value_json.co2 }}",
+            "qos": 1,
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "varhr": {
+        "type": "sensor",
+        "properties": {
+            "name": "Variation Humidite",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_varhr",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_varhr",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/qai",
+            "value_template": "{{ value_json.varhr }}",
+            "qos": 1,
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "tmpcu": {
+        "type": "sensor",
+        "properties": {
+            "name": "Temperature Cuisine",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_tmpcu",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_tmpcu",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/kitchen",
+            "value_template": "{{ value_json.tmpcu }}",
+            "min": 0,
+            "max": 100,
+            "qos": 1,
+            "device_class": "Temperature",
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "hrcu": {
+        "type": "sensor",
+        "properties": {
+            "name": "Humidite Cuisine",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_hrcu",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_hrcu",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/kitchen",
+            "value_template": "{{ value_json.hrcu }}",
+            "min": 0,
+            "max": 100,
+            "qos": 1,
+            "device_class": "Humidity",
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "tmpba1": {
+        "type": "sensor",
+        "properties": {
+            "name": "Temperature Salle de bain 1",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_tmpba1",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_tmpba1",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/bathroom",
+            "value_template": "{{ value_json.tmpba1 }}",
+            "min": 0,
+            "max": 100,
+            "qos": 1,
+            "device_class": "Temperature",
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "hrba1": {
+        "type": "sensor",
+        "properties": {
+            "name": "Humidite Salle de bain 1",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_hrba1",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_hrba1",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/bathroom",
+            "value_template": "{{ value_json.hrba1 }}",
+            "min": 0,
+            "max": 100,
+            "qos": 1,
+            "device_class": "Humidity",
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "tmpba2": {
+        "type": "sensor",
+        "properties": {
+            "name": "Temperature Salle de bain 2",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_tmpba2",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_tmpba2",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/bathroom",
+            "value_template": "{{ value_json.tmpba2 }}",
+            "min": 0,
+            "max": 100,
+            "qos": 1,
+            "device_class": "Temperature",
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
+    "hrba2": {
+        "type": "sensor",
+        "properties": {
+            "name": "Humidite Salle de bain 2",
+            "unique_id": UALDES_OPTIONS["serial_number"]+"_hrba2",
+            "object_id": UALDES_OPTIONS["serial_number"]+"_hrba2",
+            "state_topic": MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/bathroom",
+            "value_template": "{{ value_json.hrba2 }}",
+            "min": 0,
+            "max": 100,
+            "qos": 1,
+            "device_class": "Humidity",
+            "device": {
+                "identifiers": [
+                    UALDES_OPTIONS["serial_number"]
+                ],
+                "name": UALDES_OPTIONS["device"],
+                "manufacturer": "Aldes",
+                "model": "EASY_HOME",
+            },
+        }
+    },
 }
--- Software/Raspberry Pico W/uAldes/main.py
+++ Software/Raspberry Pico W/uAldes/main.py
@@ -29,15 +29,32 @@ SOFTWARE.
 from machine import Pin, UART, reset
 import utime
 import network, rp2
+import json
 
 #from umqttsimple import MQTTClient
 from simple import MQTTClient
 
-import ualdes
-from config import MQTT_CONFIG,MQTT_TOPICS, WIFI_NETWORKS,UALDES_OPTIONS
+#import ualdes
+from ualdes import ITEMS_MAPPING, FRAME_INFO, frame_decode, frame_encode
 
-RELEASE_DATE = "20_05_2025"
-VERSION = "2.1"
+from config import MQTT_CONFIG, MQTT_TOPICS, MQTT_GROUPS, MQTT_HA, WIFI_NETWORKS, UALDES_OPTIONS
+
+RELEASE_DATE = "02_02_2026"
+VERSION = "2.2"
+# VERSION 2.2 implement EASYHOME VMC
+
+# Circular Buffer to capture UART
+RX_BUFFER_SIZE = 1024
+rx_buffer = bytearray(RX_BUFFER_SIZE)
+rx_write_pos = 0
+rx_read_pos = 0
+start = None
+
+# Statistics
+stats_frames_ok = 0
+stats_frames_bad_checksum = 0
+stats_buffer_overflow = 0
+stats_frames_skipped = 0
 
 # Example of serial input format
 example_serial_input = [0x33, 0xff, 0x4c, 0x33, 0x26, 0x00, 0x01, 0x01, 0x98, 0x03, 0x00, 0x00, 0x88, 0x00, 0x00, 0x28, 
@@ -48,8 +65,17 @@ example_serial_input = [0x33, 0xff, 0x4c, 0x33, 0x26, 0x00, 0x01, 0x01, 0x98, 0x
 
 print(f"Release Date : {RELEASE_DATE}")
 
-# UART to STM32 setup :   
-uart = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1))
+if UALDES_OPTIONS["device"] == "T.Flow":
+    # UART to STM32 setup :   
+    uart = UART(0, baudrate=115200, tx=Pin(0), rx=Pin(1))
+    print("Info: ALDES DEVICE: T.Flow")
+elif UALDES_OPTIONS["device"] == "EASYHOME":
+    # UART to VMC setup :
+    uart = UART(0, baudrate=2400, bits=8, parity=0, stop=1, 
+             tx=Pin(0), rx=Pin(1))
+    print("Info: ALDES DEVICE: EASYHOME")
+else:
+    print(f"Configuration error: ALDES DEVICE not recognized: ", UALDES_OPTIONS["device"])
 
 # Software variables : 
 last_message = 0
@@ -93,7 +119,77 @@ def try_reconnect(max_attempts=5):
     print("Reconnexion impossible. Redémarrage du système.")
     reset()
 
+def uart_to_buffer():
+    global rx_write_pos, rx_read_pos, stats_buffer_overflow
+    
+    if not uart.any():
+        return 0
+    
+    bytes_written = 0
+    
+    while uart.any():
+        space_available = (rx_read_pos - rx_write_pos - 1) % RX_BUFFER_SIZE
+        if space_available == 0:
+            stats_buffer_overflow += 1
+            print("Buffer overflow!")
+            #break
+        
+        #read_size = min(space_available, uart.any())
+        read_size = uart.any()
+        data = uart.read(read_size)
+        
+        if not data:
+            break
+        
+        for b in data:
+            rx_buffer[rx_write_pos] = b
+            rx_write_pos = (rx_write_pos + 1) % RX_BUFFER_SIZE
+            bytes_written += 1
+    
+    return bytes_written
 
+def extract_frame_from_buffer():
+    global rx_read_pos, rx_write_pos, rx_buffer, start
+    available = (rx_write_pos - rx_read_pos) % RX_BUFFER_SIZE
+    if available < 2:
+        return None
+    
+    # Looking for RX_START_IDENTIFIER if not found
+    if start is None:
+        for i in range(0, available, 2):
+            idx = (rx_read_pos + i) % RX_BUFFER_SIZE
+            if rx_buffer[idx] == FRAME_INFO["RX_MASTER_IDENTIFIER"] and rx_buffer[(idx + 1)% RX_BUFFER_SIZE] == FRAME_INFO["RX_SLAVE_IDENTIFIER"]:
+                start = idx
+                break
+    
+    # If start is still not found, move the rx position to last position in the buffer and come back later
+    if start is None:
+        rx_read_pos = rx_write_pos
+        return None
+    
+    # The buffer need to be at least equal to data start + ITEM_MAPPING["Data_Lenght"]["Index"]
+    Data_Lenght_Position = start + ITEMS_MAPPING["Data_Lenght"]["Index"]
+    #print ("Data_Lenght_Position: ", Data_Lenght_Position)
+    #print ("rx_read_pos: ", rx_read_pos)
+    #print ("available: ", available)
+    if (rx_read_pos + available - 1) < Data_Lenght_Position:
+        return None
+    
+    # Looking for Data_Lenght
+    frame_size = rx_buffer[Data_Lenght_Position]
+    
+    # Verify if the entire frame is in the buffer + the checksum
+    end = start + frame_size + 1
+    if (rx_read_pos + available) < end:
+        return None
+    
+    frame = bytearray(frame_size + 1)
+    # Extract then entire frame
+    for j in range(frame_size + 1):
+        frame[j] = rx_buffer[(start + j) % RX_BUFFER_SIZE]
+    rx_read_pos = (start + j + 1) % RX_BUFFER_SIZE
+    start = None
+    return bytes(frame)
 
 def connect_and_subscribe():
   global client
@@ -109,7 +205,7 @@ def sub_cb(topic, msg):
   if topic == (MQTT_TOPICS["command"].encode()):
     led.off()
     print('Received command: %s' % msg)
-    input_cmd = ualdes.frame_encode(msg)
+    input_cmd = frame_encode(msg)
     print(input_cmd)
     if input_cmd != None:      
        print(uart.write(bytearray(input_cmd)))
@@ -121,11 +217,10 @@ client = None
 try_reconnect()
 
 last_ping = utime.time()
-ping_interval = 30  # Ping toutes les 30 secondes
-
 
 while True:
   # Vérification périodique de la connexion Wi-Fi
+  uart_data = None
   if not wlan.isconnected():
     print("Wi-Fi déconnecté. Tentative de reconnexion...")
     wlan.connect(WIFI_NETWORKS["ssid"], WIFI_NETWORKS["password"])
@@ -141,10 +236,14 @@ while True:
 
   try:
     client.check_msg()
-    uart_data = uart.read()
+    
+    if UALDES_OPTIONS["device"] == "T.Flow":
+        uart_data = uart.read()
+    elif UALDES_OPTIONS["device"] == "EASYHOME":
+        uart_to_buffer()
+        uart_data = extract_frame_from_buffer()
 
-
-    if (utime.time() - last_ping) > ping_interval:
+    if (utime.time() - last_ping) > MQTT_CONFIG["keepalive"]:
         try:
             client.ping()
             print("Ping envoyé")
@@ -153,19 +252,54 @@ while True:
             print("Erreur ping, tentative de reconnexion...")
             try_reconnect()
 
-    if (utime.time() - last_message) > UALDES_OPTIONS["refresh_time"]:
+    if UALDES_OPTIONS["device"] == "T.Flow":
+        if (utime.time() - last_message) > UALDES_OPTIONS["refresh_time"]:
+            if uart_data is not None:
+                print("Trame recue")
+                print(uart_data)
+                print("Taille : " + str(len(uart_data)))
+                try:
+                    led.off()
+                    client.publish(MQTT_TOPICS["main"]+"trame", bytearray(uart_data).hex(" "))
+                    decoded_data = frame_decode(uart_data)
+                    if decoded_data is not None:  # Check if data was decoded successfully
+                        for topic in decoded_data:
+                            client.publish(MQTT_TOPICS["main"]+topic, str(decoded_data[topic]))
+                            print(f"{MQTT_TOPICS['main']}{topic}: {decoded_data[topic]}")
+
+                    last_message = utime.time()
+                    utime.sleep(0.2)
+                    led.on()
+                except Exception as e:
+                    print("Error publishing data:", e)
+            
+    elif UALDES_OPTIONS["device"] == "EASYHOME":
         if uart_data is not None:
-            print("Trame recue")
+            print("Trame received at: ", utime.time())
             print(uart_data)
             print("Taille : " + str(len(uart_data)))
             try:
                 led.off()
-                client.publish(MQTT_TOPICS["main"]+"trame", bytearray(uart_data).hex(" "))
-                decoded_data = ualdes.frame_decode(uart_data)
+                client.publish(MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/trame", bytearray(uart_data).hex(" "))
+                decoded_data = frame_decode(uart_data)
                 if decoded_data is not None:  # Check if data was decoded successfully
-                    for topic in decoded_data:
-                        client.publish(MQTT_TOPICS["main"]+topic, str(decoded_data[topic]))
-                        print(f"{MQTT_TOPICS['main']}{topic}: {decoded_data[topic]}")
+                    for group, properties in MQTT_GROUPS.items():
+                        payload = "{"
+                        for topic, data in properties.items():
+                            # Add a comma in case it is not the first one
+                            if payload != "{":
+                                payload = payload+","
+                            payload = payload+"\""+topic+"\":"+str(decoded_data[data])+""
+                        payload = payload+"}"
+                        client.publish(MQTT_TOPICS["main"]+UALDES_OPTIONS["serial_number"]+"/"+group, payload)
+                        #print(f"{MQTT_TOPICS['main']}{group}: \{{topic}: {decoded_data[topic]}\}")
+                        uart_data = None
+                    if UALDES_OPTIONS["device_in_ha"]:
+                        for sensor, info in MQTT_HA.items():
+                            payload = json.dumps(info["properties"])
+                            client.publish(MQTT_TOPICS["haPrefix"]+info["type"]+"/aldes/"+UALDES_OPTIONS["serial_number"]+"_"+str(sensor)+"/config", payload)
+                            #print(f"{MQTT_TOPICS['main']}{group}: \{{topic}: {decoded_data[topic]}\}")
+                            uart_data = None
                 last_message = utime.time()
                 utime.sleep(0.2)
                 led.on()
